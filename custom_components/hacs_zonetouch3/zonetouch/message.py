99f86647480b0304bdef1ee030299143477084be
dsmackie <48046804+dsmackie@users.noreply.github.com>
2025-07-19 04:30:14 +0000

    Refactor message processing

--- custom_components/hacs_zonetouch3/zonetouch/message.py
+++ custom_components/hacs_zonetouch3/zonetouch/message.py
@@ -31,7 +31,46 @@ class ZoneTouchMessage:
 
         self.data = data
         if self.__validate():
-            self.__unpack()
+            self.__unpack_header()
+
+            match self.message_type:
+                case MessageType.MESSAGE_TYPE_SUBCOMMAND:
+                    (sub_message_type,) = struct.unpack_from(">B", self.data, 10)
+                    self.sub_message_type = Response(sub_message_type)
+                    (_, length, count) = struct.unpack_from(">HHH", self.data, 12)
+                    self.message_data = self.data[18:-2]
+
+                    match self.sub_message_type:
+                        case Response.RESPONSE_GROUP_CONTROL:
+                            _LOGGER.debug("RESPONSE_GROUP_CONTROL")
+                            self.__parseGroupControl(self.message_data, count)
+                        case Response.RESPONSE_GROUP_NAME:
+                            _LOGGER.debug("RESPONSE_GROUP_NAME")
+                        case Response.RESPONSE_FAVOURITE:
+                            _LOGGER.debug("RESPONSE_FAVOURITE")
+                        case Response.RESPONSE_PROGRAM:
+                            _LOGGER.debug("RESPONSE_PROGRAM")
+                        case Response.RESPONSE_ZONE_INFO:
+                            _LOGGER.debug("RESPONSE_ZONE_INFO")
+                        case Response.RESPONSE_GROUPING:
+                            _LOGGER.debug("RESPONSE_GROUPING")
+                        case Response.RESPONSE_SERVICE:
+                            _LOGGER.debug("RESPONSE_SERVICE")
+                        case Response.RESPONSE_PASSWORD:
+                            _LOGGER.debug("RESPONSE_PASSWORD")
+                        case Response.RESPONSE_NOTIFICATION:
+                            _LOGGER.debug("RESPONSE_NOTIFICATION")
+                        case Response.RESPONSE_SENSOR:
+                            _LOGGER.debug("RESPONSE_SENSOR")
+                            self.__unpack_sensor(count, length)
+                        case Response.RESPONSE_PREFERENCE:
+                            _LOGGER.debug("RESPONSE_PREFERENCE")
+                        case Response.RESPONSE_PARAMETERS:
+                            _LOGGER.debug("RESPONSE_PARAMETERS")
+                        case _:
+                            _LOGGER.debug("RESPONSE_UNKNOWN")
+                case _:
+                    self.message_data = self.data[11:-2]
 
         _LOGGER.debug(Address(self.addrDest))
         _LOGGER.debug(Address(self.addrSrc))
@@ -40,35 +79,12 @@ class ZoneTouchMessage:
         _LOGGER.debug(self.data.hex())
         _LOGGER.debug(self.message_data.hex())
 
-        match self.sub_message_type:
-            case Response.RESPONSE_GROUP_CONTROL:
-                _LOGGER.debug("RESPONSE_GROUP_CONTROL")
-            case Response.RESPONSE_GROUP_NAME:
-                _LOGGER.debug("RESPONSE_GROUP_NAME")
-            case Response.RESPONSE_FAVOURITE:
-                _LOGGER.debug("RESPONSE_FAVOURITE")
-            case Response.RESPONSE_PROGRAM:
-                _LOGGER.debug("RESPONSE_PROGRAM")
-            case Response.RESPONSE_ZONE_INFO:
-                _LOGGER.debug("RESPONSE_ZONE_INFO")
-            case Response.RESPONSE_GROUPING:
-                _LOGGER.debug("RESPONSE_GROUPING")
-            case Response.RESPONSE_SERVICE:
-                _LOGGER.debug("RESPONSE_SERVICE")
-            case Response.RESPONSE_PASSWORD:
-                _LOGGER.debug("RESPONSE_PASSWORD")
-            case Response.RESPONSE_NOTIFICATION:
-                _LOGGER.debug("RESPONSE_NOTIFICATION")
-            case Response.RESPONSE_SENSOR:
-                _LOGGER.debug("RESPONSE_SENSOR")
-            case Response.RESPONSE_PREFERENCE:
-                _LOGGER.debug("RESPONSE_PREFERENCE")
-            case Response.RESPONSE_PARAMETERS:
-                _LOGGER.debug("RESPONSE_PARAMETERS")
-            case _:
-                _LOGGER.debug("RESPONSE_UNKNOWN")
-
     def __validate(self) -> bool:
+        """Validate received data.
+
+        Each packet received from the controller contains a modbus checksum as the last 2 bytes.
+        The data is hashed and the checksum validated against the expected checksum.
+        """
         data_msg = self.data[4:-2]
         crc_orig = self.data[-2:]
         crc_check = modbus_crc.crc16(data_msg)
@@ -76,8 +92,12 @@ class ZoneTouchMessage:
         self.valid = crc_orig[0] == crc_check[1] and crc_orig[1] == crc_check[0]
         return self.valid
 
-    def __unpack(self) -> None:
-        """Unpack message into each component."""
+    def __unpack_header(self) -> None:
+        """Process received data header.
+
+        Each packet has a header that defines what command the packet contains.
+        We process the header so we know how to handle the received command.
+        """
         (
             self.header,
             self.addrDest,
@@ -88,25 +108,14 @@ class ZoneTouchMessage:
         ) = struct.unpack(">IBBBBH", self.data[:10])
         self.message_type = MessageType(message_type)
 
-        match self.message_type:
-            case MessageType.MESSAGE_TYPE_SUBCOMMAND:
-                (sub_message_type,) = struct.unpack_from(">B", self.data, 10)
-                self.sub_message_type = Response(sub_message_type)
-                self.message_data = self.data[12:-2]
-                (_, length, count) = struct.unpack_from(">HHH", self.data, 12)
-                match self.sub_message_type:
-                    case Response.RESPONSE_SENSOR:
-                        for x in range(count):
-                            (addr, _, temperature) = struct.unpack_from(
-                                ">BBH", self.data, 18 + (length * x)
-                            )
-                            if addr == 159 and temperature >= 0:
-                                self.temperature = (temperature - 500) / 10
-                    case Response.RESPONSE_GROUP_CONTROL:
-                        self.__parseGroupControl(self.data[18:], count)
-
-            case _:
-                self.message_data = self.data[11:-2]
+    def __unpack_sensor(self, count, length) -> None:
+        """Process received temperature sensor data."""
+        for x in range(count):
+            (addr, _, temperature) = struct.unpack_from(
+                ">BBH", self.data, 18 + (length * x)
+            )
+            if addr == 159 and temperature >= 0:
+                self.temperature = (temperature - 500) / 10
 
     def __parseGroupControl(self, data, count) -> None:
         for idx in range(count):
