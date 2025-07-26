37450c95bf232019a3550f61e89347410aadfe8c
dsmackie <48046804+dsmackie@users.noreply.github.com>
2025-07-26 11:31:06 +0000

    Tidy spill sensor code

--- custom_components/hacs_zonetouch3/binary_sensor.py
+++ custom_components/hacs_zonetouch3/binary_sensor.py
@@ -1,9 +1,7 @@
-from homeassistant.components.binary_sensor import (
-    BinarySensorDeviceClass,
-    BinarySensorEntity,
-    BinarySensorEntityDescription,
-)
-from homeassistant.components.sensor import dataclass
+"""Binary Sensor entity."""
+
+from config.custom_components.zonetouch3.zonetouch.group import ZoneTouch3Group
+from homeassistant.components.binary_sensor import BinarySensorEntity
 from homeassistant.const import EntityCategory
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
@@ -14,67 +12,42 @@ from .data import ZoneTouch3ConfigEntry
 from .entity import ZoneTouch3Entity
 
 
-@dataclass
-class ZoneTouch3BinarySensorEntityDescription(BinarySensorEntityDescription):
-    """A class that describes custom binary sensor entities."""
-
-    is_on: bool | None = None
-    on_icon: str | None = None
-    off_icon: str | None = None
-
-
 async def async_setup_entry(
     hass: HomeAssistant,
     entry: ZoneTouch3ConfigEntry,
     async_add_entities: AddEntitiesCallback,
 ) -> None:
     """Set up the binary sensor platform."""
-    entities: list[ZoneTouch3BinarySensor] = []
-    for group in entry.runtime_data.coordinator.data.groups.values():
-        desc = ZoneTouch3BinarySensorEntityDescription(
-            key=f"valve_{group.id}_spill",
-            name=f"{group.name} Spill",
-            is_on=group.is_spill_on,
-            on_icon="mdi:valve-open",
-            off_icon="mdi:valve-closed",
-            entity_category=EntityCategory.DIAGNOSTIC,
-        )
-        entities.append(ZoneTouch3BinarySensor(entry.runtime_data.coordinator, desc))
-    async_add_entities(entities)
+    async_add_entities(
+        ZoneTouch3GroupSpillSensor(entry.runtime_data.coordinator, group)
+        for group in entry.runtime_data.coordinator.data.groups.values()
+    )
 
 
-class ZoneTouch3BinarySensor(BinarySensorEntity, ZoneTouch3Entity):
+class ZoneTouch3GroupSpillSensor(BinarySensorEntity, ZoneTouch3Entity):
     """Hyundai / Kia Connect binary sensor class."""
 
     def __init__(
         self,
         coordinator: ZoneTouch3DataUpdateCoordinator,
-        description: ZoneTouch3BinarySensorEntityDescription,
+        group: ZoneTouch3Group,
     ) -> None:
         """Initialize the sensor."""
         super().__init__(coordinator)
-        self.entity_description: ZoneTouch3BinarySensorEntityDescription = description
-        self._attr_unique_id = f"{DOMAIN}_{description.key}"
-        self._attr_name = f"{description.name}"
-        if description.entity_category:
-            self._attr_entity_category = description.entity_category
+        self.group = group
+        self._attr_name = f"{group.name} Spill"
+        self._attr_on_icon = ("mdi:valve-open",)
+        self._attr_off_icon = ("mdi:valve-closed",)
+        self._attr_unique_id = f"{DOMAIN}_valve_{group.id}_spill"
+        self._attr_name = f"{group.name} Spill"
+        self._attr_entity_category = EntityCategory.DIAGNOSTIC
 
     @property
     def is_on(self) -> bool | None:
         """Return true if the binary sensor is on."""
-        if self.entity_description.is_on is not None:
-            return self.entity_description.is_on
-        return None
+        return self.group.is_spill_on
 
     @property
     def icon(self):
         """Return the icon to use in the frontend, if any."""
-        if (
-            self.entity_description.on_icon == self.entity_description.off_icon
-        ) is None:
-            return BinarySensorEntity.icon
-        return (
-            self.entity_description.on_icon
-            if self.is_on
-            else self.entity_description.off_icon
-        )
+        return self._attr_on_icon if self.is_on else self._attr_off_icon
